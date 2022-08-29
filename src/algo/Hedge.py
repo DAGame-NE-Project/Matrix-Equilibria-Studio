d20d078f2b194993ecaf62ab166a927a132ca38d
rowdark <wenhanhuang1993@gmail.com>
2022-08-29 18:17:08 +0800

    modify_hedge_rm

--- src/algo/Hedge.py
+++ src/algo/Hedge.py
@@ -3,106 +3,157 @@ import numpy as np
 
 class Hedge(object):
 
-    def __init__(self,n=10,T=10,a=1):
-        """
-        Args:
-            n (int): number of experts present
-            T (int): number of time steps taken
-            a (float): value that we can use to scale our epsilon
-            epsilon (float): the theoretical episilon, but which can be customized by a
-        """
-        self.n = n
-        self.T = T
-        self.a = a
-        self.weights = np.ones(n)/n
-        self.epsilon = np.sqrt(np.log(n) / T) * a
+    def __init__(self, args):
 
-    def weight(self,expert_predictions,actual_value):
-        """
-        Will recalculate the weights on each of the experts based off of the expert predictions and the actual value.
-        This can only be done after the actual value is known.
+        self.args = args
+        if hasattr(args, 'epsilon'):
+            self.epsilon = float(args.epsilon)
+        else:
+            self.epsilon = 0
 
-        Args:
-            expert_predictions (np.array) (pred.float): np.array with the expert predictions
-            actual_values (float): float of the actual value here
+    def solve(self, game, info):
 
-        Returns:
-            Nothing
-        """
+        actions = game.getActionSpace()
+        players = game.players
+        if info is None or info['solver'] != "Hedge":
+            info = {
+                'solver': "Hedge",
+                'agents_history_log_weights': [[0.0 for action_id in range(actions[player_id])] for player_id in range(players)],
+                'overall_policy': [[0.0 for action_id in range(actions[player_id])] for player_id in range(players)],
+            }
 
-        #Calculating losses
-        losses = self.se(expert_predictions,actual_value)
+        ret = []
+        tmp_ret = tuple()
 
-        #Apply weights
-        reweighting = np.exp(-self.epsilon*losses)
-        self.weights*=reweighting
-        self.weights/=np.sum(self.weights)
+        for player_id in range(players):
+            policy = np.array(info['agents_history_log_weights'][player_id])
+            policy = np.exp(policy - np.max(policy))
+            policy = policy / np.sum(policy)
+            ret.append(policy)
+            tmp_ret = tmp_ret + tuple([int(np.random.choice(np.array(range(actions[player_id])), p = policy))])
 
-    def predict(self,expert_predictions):
-        """
-        Weights the expert predictions into a single prediction based on the weights that have been calculated by the
-        hedge algorithm
+        self._update_info(game, tmp_ret, info)
 
-        Args:
-            expert predictions (np.array) (pred.float): np.array with the expert predictions
+        for player_id in range(players):
+            info['overall_policy'][player_id] += ret[player_id]
 
-        Returns:
-            a value for prediction based on the inputs of the experts and their respective weights.
-        """
+        return ret, tmp_ret, info
 
-        return np.dot(self.weights,expert_predictions)
+    def _update_info(self, game, ret, info):
 
-    def fit_predict(self,expert_predictions,actual_values):
-        """
-        Will perform weighting at each time step of historical data in order to fit to the data and also perform online
-        predictions. Shows the performance of the algorithm and can be used to check performance.
+        actions = game.getActionSpace()
+        for player_id in range(game.players):
+            tmp_ret = list(ret)
+            for action_id in range(actions[player_id]):
+                tmp_ret[player_id] = action_id
+                u = game.playID(tmp_ret, player_id)
+                inc = np.log(1 + self.epsilon) * u
+                info['agents_history_log_weights'][player_id][action_id] += inc
 
-        Args:
-            expert_predictions (np.array) (pred.float,time.float): np.array with the expert predictions across time
-            actual_values (np.array) (time.float): np.array with the actual value across time.
+    def reset(self):
+        pass
 
-        Returns:
-            (weights (np.array), predictions (np.array))
-        """
-        weights = []
-        predictions = []
-        for i in range(len(actual_values)):
-            weights.append(hedge.weights.copy())
-            predictions.append(hedge.predict(expert_predictions[:,i]))
-            hedge.re_weight(expert_predictions[:,i],actual_values[i])
 
-        return (np.array(weights), np.array(predictions))
-
-    def se(actual,expected):
-        """
-        Will return the squared error between the two arguments
-        """
-        return np.power(actual-expected,2)
-    
-    def run():
-        """
-        run to update the weights
-        
-        Return:
-            updated weights
-        """
-        for i in range(self.T):
-            weights.append(self.weights.copy())
-            self.weight(expert_predictions[:,i],actual_values[i])
-        
-        return self.weight
-    
-if __name__ == '__main__':
-
-    actual_values = np.random.randint(0,10,size=[20])
-
-    # Fixed Standard Deviations for each expert
-    stds = [1,0.1,0.03,0.5,1.3]
-    expert_predictions = np.array([actual_values+np.random.randn(actual_values.size)*std for std in stds])
-
-    hedge = onlineHedge(n=5,T=20,a=1)
-    #run to update weights
-    
-    hedge.run()
-    predictions = []
-    predictions.append(hedge.predict(expert_predictions[:]))
+#    def __init__(self,n=10,T=10,a=1):
+#        """
+#        Args:
+#            n (int): number of experts present
+#            T (int): number of time steps taken
+#            a (float): value that we can use to scale our epsilon
+#            epsilon (float): the theoretical episilon, but which can be customized by a
+#        """
+#        self.n = n
+#        self.T = T
+#        self.a = a
+#        self.weights = np.ones(n)/n
+#        self.epsilon = np.sqrt(np.log(n) / T) * a
+#
+#    def weight(self,expert_predictions,actual_value):
+#        """
+#        Will recalculate the weights on each of the experts based off of the expert predictions and the actual value.
+#        This can only be done after the actual value is known.
+#
+#        Args:
+#            expert_predictions (np.array) (pred.float): np.array with the expert predictions
+#            actual_values (float): float of the actual value here
+#
+#        Returns:
+#            Nothing
+#        """
+#
+#        #Calculating losses
+#        losses = self.se(expert_predictions,actual_value)
+#
+#        #Apply weights
+#        reweighting = np.exp(-self.epsilon*losses)
+#        self.weights*=reweighting
+#        self.weights/=np.sum(self.weights)
+#
+#    def predict(self,expert_predictions):
+#        """
+#        Weights the expert predictions into a single prediction based on the weights that have been calculated by the
+#        hedge algorithm
+#
+#        Args:
+#            expert predictions (np.array) (pred.float): np.array with the expert predictions
+#
+#        Returns:
+#            a value for prediction based on the inputs of the experts and their respective weights.
+#        """
+#
+#        return np.dot(self.weights,expert_predictions)
+#
+#    def fit_predict(self,expert_predictions,actual_values):
+#        """
+#        Will perform weighting at each time step of historical data in order to fit to the data and also perform online
+#        predictions. Shows the performance of the algorithm and can be used to check performance.
+#
+#        Args:
+#            expert_predictions (np.array) (pred.float,time.float): np.array with the expert predictions across time
+#            actual_values (np.array) (time.float): np.array with the actual value across time.
+#
+#        Returns:
+#            (weights (np.array), predictions (np.array))
+#        """
+#        weights = []
+#        predictions = []
+#        for i in range(len(actual_values)):
+#            weights.append(hedge.weights.copy())
+#            predictions.append(hedge.predict(expert_predictions[:,i]))
+#            hedge.re_weight(expert_predictions[:,i],actual_values[i])
+#
+#        return (np.array(weights), np.array(predictions))
+#
+#    def se(actual,expected):
+#        """
+#        Will return the squared error between the two arguments
+#        """
+#        return np.power(actual-expected,2)
+#    
+#    def run():
+#        """
+#        run to update the weights
+#        
+#        Return:
+#            updated weights
+#        """
+#        for i in range(self.T):
+#            weights.append(self.weights.copy())
+#            self.weight(expert_predictions[:,i],actual_values[i])
+#        
+#        return self.weight
+#    
+#if __name__ == '__main__':
+#
+#    actual_values = np.random.randint(0,10,size=[20])
+#
+#    # Fixed Standard Deviations for each expert
+#    stds = [1,0.1,0.03,0.5,1.3]
+#    expert_predictions = np.array([actual_values+np.random.randn(actual_values.size)*std for std in stds])
+#
+#    hedge = onlineHedge(n=5,T=20,a=1)
+#    #run to update weights
+#    
+#    hedge.run()
+#    predictions = []
+#    predictions.append(hedge.predict(expert_predictions[:]))
--- src/algo/RM.py
+++ src/algo/RM.py
@@ -1,92 +1,142 @@
 import numpy as np
 
-
 class Player:
-    def __init__(self, experts):
-        self.sum_p = np.full(3, 0.)
-        # total game number
-        self.games_played = 0
-        # number of experts
-        self.n = len(experts)
-        # experts list
-        self.experts = experts
-        # cumulative expected reward for our Regret Matching algorithm
-        self.expected_reward = 0.
-        # cumulative expected rewards for experts
-        self.experts_rewards = np.zeros(self.n)
-        # cumulative regrets towards experts
-        self.regrets = np.zeros(self.n)
-        # probability disribution over experts to draw decision from
-        self.p = np.full(self.n, 1. / self.n)
 
-    def move(self):
-        action = np.random.choice(self.experts, 1,  p=self.p)
-        return action[0]
-    
-    def _update_rule(self, rewards_vector):
-        self.expected_reward += np.dot(self.p, rewards_vector)
-        self.experts_rewards += rewards_vector
-        self.regrets = self.experts_rewards - self.expected_reward
-        self._update_p()
-        
-    def _update_p(self):
-        sum_w = np.sum([self._w(i) for i in np.arange(self.n)])
-        if sum_w <= 0:
-            self.p = np.full(self.n, 1. / self.n)
+    def __init__(self, args):
+
+        self.args = args
+        if hasattr(args, 'epsilon'):
+            self.epsilon = float(args.epsilon)
         else:
-            self.p = np.asarray(
-                [self._w(i) / sum_w for i in np.arange(self.n)]
-            )
+            self.epsilon = 0
 
-    def _w(self, i):
-        return max(0, self.regrets[i])
+    def solve(self, game, info):
 
-    def learn_from(self, opponent_move):
-        reward_vector = REWARD_VECTORS[opponent_move]
-        self._update_rule(reward_vector)
-        self.games_played += 1
-        self.sum_p += self.p
+        actions = game.getActionSpace()
+        players = game.players
+        if info is None or info['solver'] != "RegretMatching":
+            info = {
+                'solver': "RegretMatching",
+                'agents_regrets': [[0.0 for action_id in range(actions[player_id])] for player_id in range(players)],
+                'overall_policy': [[0.0 for action_id in range(actions[player_id])] for player_id in range(players)],
+            }
 
-    def current_best_response(self):
-        return np.round(self.sum_p / self.games_played, 4)
+        ret = []
+        tmp_ret = tuple()
 
-    def eps(self):
-        return np.max(self.regrets / self.games_played)
- 
-class Nash(object):
-    def __init__(self, p0, p1):
-        self.p0 = p0
-        self.p1 = p1
+        for player_id in range(players):
+            policy = np.array(info['agents_regrets'][player_id])
+            policy = np.expand_dims(policy, axis = 1).max(axis = 1, initial = 0)
+            policy = policy / np.sum(policy)
+            ret.append(policy)
+            tmp_ret = tmp_ret + tuple([int(np.random.choice(np.array(range(actions[player_id])), p = policy))])
 
-    def getActionSpace(self, iterations):
-        """
-        求解纳什均衡
-        :param  iterations: 迭代次数
-        """
-        for i in range(0, iterations):
-            p0_move = self.p0.move()
-            p1_move = self.p1.move()
-            self.p0.learn_from(p0_move)
-            self.p1.learn_from(p1_move)
+        self._update_info(game, tmp_ret, info)
 
-        _2e = np.round(2 * np.max([self.p0.eps(), self.p1.eps()]), 3)
-        p0_ne = self.p0.current_best_response()
-        p1_ne = self.p1.current_best_response()
-        print("{0} - nash equilibrium for RPS: {1}, {2}".format(_2e, p0_ne, p1_ne))
+        for player_id in range(players):
+            info['overall_policy'][player_id] += ret[player_id]
 
-        
-"""
-if __name__ == '__main__':
-    ACTION = ['ROCK', 'PAPER', 'SCISSORS']
+        return ret, tmp_ret, info
 
-    REWARD_VECTORS = {
-    'ROCK':     np.asarray([0, 1, -1]),  # opponent playing ROCK
-    'PAPER':    np.asarray([-1, 0, 1]),  # opponent playing PAPER
-    'SCISSORS': np.asarray([1, -1, 0]),  # opponent playing PAPER
-}
-    a = Player(ACTION)
-    b = Player(ACTION)
-    t = 10000
-    nash=Nash(a,b)
-    nash.getActionSpace(t)
-"""
+    def _update_info(self, game, ret, info):
+
+        actions = game.getActionSpace()
+        for player_id in range(game.players):
+            tmp_ret = list(ret)
+            tmp_rewards = []
+            for action_id in range(actions[player_id]):
+                tmp_ret[player_id] = action_id
+                u = game.playID(tmp_ret, player_id)
+                tmp_rewards.append(u)
+            expected_rewards = np.array(tmp_rewards).dot(ret[player_id])
+            for action_id in range(actions[player_id]):
+                info['agents_regrets'][player_id][action_id] = tmp_rewards[action_id] - expected_rewards
+
+#    def __init__(self, experts):
+#        self.sum_p = np.full(3, 0.)
+#        # total game number
+#        self.games_played = 0
+#        # number of experts
+#        self.n = len(experts)
+#        # experts list
+#        self.experts = experts
+#        # cumulative expected reward for our Regret Matching algorithm
+#        self.expected_reward = 0.
+#        # cumulative expected rewards for experts
+#        self.experts_rewards = np.zeros(self.n)
+#        # cumulative regrets towards experts
+#        self.regrets = np.zeros(self.n)
+#        # probability disribution over experts to draw decision from
+#        self.p = np.full(self.n, 1. / self.n)
+#
+#    def move(self):
+#        action = np.random.choice(self.experts, 1,  p=self.p)
+#        return action[0]
+#    
+#    def _update_rule(self, rewards_vector):
+#        self.expected_reward += np.dot(self.p, rewards_vector)
+#        self.experts_rewards += rewards_vector
+#        self.regrets = self.experts_rewards - self.expected_reward
+#        self._update_p()
+#        
+#    def _update_p(self):
+#        sum_w = np.sum([self._w(i) for i in np.arange(self.n)])
+#        if sum_w <= 0:
+#            self.p = np.full(self.n, 1. / self.n)
+#        else:
+#            self.p = np.asarray(
+#                [self._w(i) / sum_w for i in np.arange(self.n)]
+#            )
+#
+#    def _w(self, i):
+#        return max(0, self.regrets[i])
+#
+#    def learn_from(self, opponent_move):
+#        reward_vector = REWARD_VECTORS[opponent_move]
+#        self._update_rule(reward_vector)
+#        self.games_played += 1
+#        self.sum_p += self.p
+#
+#    def current_best_response(self):
+#        return np.round(self.sum_p / self.games_played, 4)
+#
+#    def eps(self):
+#        return np.max(self.regrets / self.games_played)
+# 
+#class Nash(object):
+#    def __init__(self, p0, p1):
+#        self.p0 = p0
+#        self.p1 = p1
+#
+#    def getActionSpace(self, iterations):
+#        """
+#        求解纳什均衡
+#        :param  iterations: 迭代次数
+#        """
+#        for i in range(0, iterations):
+#            p0_move = self.p0.move()
+#            p1_move = self.p1.move()
+#            self.p0.learn_from(p0_move)
+#            self.p1.learn_from(p1_move)
+#
+#        _2e = np.round(2 * np.max([self.p0.eps(), self.p1.eps()]), 3)
+#        p0_ne = self.p0.current_best_response()
+#        p1_ne = self.p1.current_best_response()
+#        print("{0} - nash equilibrium for RPS: {1}, {2}".format(_2e, p0_ne, p1_ne))
+#
+#        
+#"""
+#if __name__ == '__main__':
+#    ACTION = ['ROCK', 'PAPER', 'SCISSORS']
+#
+#    REWARD_VECTORS = {
+#    'ROCK':     np.asarray([0, 1, -1]),  # opponent playing ROCK
+#    'PAPER':    np.asarray([-1, 0, 1]),  # opponent playing PAPER
+#    'SCISSORS': np.asarray([1, -1, 0]),  # opponent playing PAPER
+#}
+#    a = Player(ACTION)
+#    b = Player(ACTION)
+#    t = 10000
+#    nash=Nash(a,b)
+#    nash.getActionSpace(t)
+#"""
