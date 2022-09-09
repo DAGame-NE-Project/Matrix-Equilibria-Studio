e34e6a1c182edfac19bc36cf8ad12935d1e6078f
lhydave <lhydave@163.com>
2022-09-09 14:21:45 +0800

    change names.

--- src/test.py
+++ src/test.py
@@ -1,18 +1,31 @@
 import numpy as np
 import numpy as np
-from algo.TS import *
+from algo.CDFFJS_6528 import *
 
 if __name__ == '__main__':
-    # test tight instance
-    eps = 1e-6
-    R = np.array([[0, 0, 0], [1/3, 1, 1], [1/3, 2/3-eps/2, 2/3-eps/2]])
-    C = np.array([[0, 1/3-eps, 1/3-eps], [0, 1, 2/3+eps], [0, 1, 2/3+eps]])
-    init_x = np.array([1.0, 0, 0])
-    init_y = np.array([1.0, 0, 0])
-    x, y = solve(R, C, adjust_method='DFM',
-                 line_search_method='adapted', init_x=init_x, init_y=init_y)
-    print(calculate_f_value(R, C, x, y))
+    R = np.array([[0.01, 0, 0], [0.01+0.3393, 1, 1]])
+    C = np.array([[0.01, 0.01+0.3393, 0.01+0.3393],
+                 [0, 1, 0.812815]])
+    print(solve(R, C))
     R, C = C.transpose(), R.transpose()
-    x, y = solve(R, C, adjust_method='DFM',
-                 line_search_method='adapted', init_x=init_x, init_y=init_y)
-    print(calculate_f_value(R, C, x, y))
+    print(solve(R, C))
+    R = np.array([[0.01, 0, 0], [0.01+0.3393, 1, 1],
+                 [0.01+0.3393, 0.582523, 0.582523]])
+    C = np.array([[0.01, 0.01+0.3393, 0.01+0.3393],
+                 [0, 1, 0.812815], [0, 1, 0.812815]])
+    print(solve(R, C))
+
+    # test other cases
+    R = np.array([[0.4, 0, 0], [0.4+0.3393, 1, 1]])
+    C = np.array([[0.4, 0.4+0.3393, 0.4+0.3393],
+                 [0, 1, 0.812815]])
+    print(solve(R, C))
+    R, C = C.transpose(), R.transpose()
+    print(solve(R, C))
+    R = np.array([[0.4, 0, 0], [0.4+0.3393, 1, 1],
+                 [0.4+0.3393, 0.582523, 0.582523]])
+    C = np.array([[0.4, 0.4+0.3393, 0.4+0.3393],
+                 [0, 1, 0.812815], [0, 1, 0.812815]])
+    print(solve(R, C))
+    R, C = C.transpose(), R.transpose()
+    print(solve(R, C))
