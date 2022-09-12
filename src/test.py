48298aee6043c246ce102440e3d9b8d7527d19bb
lhydave <lhydave@163.com>
2022-09-12 15:36:19 +0800

    k_uniform_search finished and tested..

--- src/test.py
+++ src/test.py
@@ -1,28 +1,31 @@
 import numpy as np
-from algo.TS import *
+from algo.k_uniform_search import *
 
 if __name__ == '__main__':
-    # test tight instance of DFM
     from env import matrixgame
     from env.gen.randommatrixgenerator import RandomMatrixGenerator
     import argparse
-    args = argparse.Namespace()
-    eps = 1e-6
-    R = np.array([[0, 0, 0], [1/3, 1, 1], [1/3, 2/3-eps/2, 2/3-eps/2]])
-    C = np.array([[0, 1/3-eps, 1/3-eps], [0, 1, 2/3+eps], [0, 1, 2/3+eps]])
-    args.players = 2
-    args.actionspace = [R.shape[0], R.shape[1]]
-    gen = RandomMatrixGenerator(args)
-    game = matrixgame.MatrixGame(gen)
-    init_x = np.array([1.0, 0, 0])
-    init_y = np.array([1.0, 0, 0])
-    args.init_x = init_x
-    args.init_y = init_y
-    p = Player(args=args)
-    x, y = p.solve(game, [R, C])
-    print(x, y)
-    print(calculate_f_value(R, C, x[0], x[1]))
-    R, C = C.transpose(), R.transpose()
-    x, y = p.solve(game, [R, C])
-    print(x, y)
-    print(calculate_f_value(R, C, x[0], x[1]))
+
+    def test_RC(R, C, goal):
+        args = argparse.Namespace()
+        args.players = 2
+        args.actionspace = [R.shape[0], R.shape[1]]
+        gen = RandomMatrixGenerator(args)
+        game = matrixgame.MatrixGame(gen)
+        args.goal = goal
+        p = Player(args=args)
+        print(p.solve(game, [R, C]))
+
+    R = np.array([[1, 0, 0], [0, 1, 0]])
+    C = np.array([[0, 1, 0], [1, 0, 0]])
+    # test exhaustive_search
+    test_RC(R, C, 'epsNE')
+    test_RC(R, C, 'epsWSNE')
+    R = C
+    test_RC(R, C, 'epsNE')
+    test_RC(R, C, 'epsWSNE')
+    # paper-scissors-rock
+    R = np.array([[0.5, 1, 0], [0, 0.5, 1], [1, 0, 0.5]])
+    C = 1 - R
+    test_RC(R, C, 'epsNE')
+    test_RC(R, C, 'epsWSNE')
